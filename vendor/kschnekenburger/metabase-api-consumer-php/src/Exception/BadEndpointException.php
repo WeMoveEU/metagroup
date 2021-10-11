a8f8231fc95c8146e613e9b3cbb7d1bd97f36fd1
Romain Thouvenin <romain@wemove.eu>
2021-10-11 14:36:26 +0200

    Remove dependencies code from git

--- vendor/kschnekenburger/metabase-api-consumer-php/src/Exception/BadEndpointException.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace KenSh\MetabaseApi\Exception;
-
-class BadEndpointException extends \Exception implements MetabaseApiExceptionInterface
-{
-
-}
--- vendor/kschnekenburger/metabase-api-consumer-php/src/Exception/ResourceNotFoundException.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace KenSh\MetabaseApi\Exception;
-
-class ResourceNotFoundException extends \Exception implements MetabaseApiExceptionInterface
-{
-
-}
